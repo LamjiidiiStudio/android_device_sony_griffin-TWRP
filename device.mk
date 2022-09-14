b9ff03a1ef298b5cbe1bae29489f81417ec1bc08
lamjiidii1 <lamjiidii1@gmail.com>
2022-09-14 16:07:14 +0100

    griffin: Import Boot control HAL

--- device.mk
+++ device.mk
@@ -28,6 +28,33 @@ LOCAL_PATH := device/sony/griffin
 # define hardware platform
 PRODUCT_PLATFORM := msmnile
 
+# A/B support
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service \
+    android.hardware.boot@1.0-impl-wrapper.recovery \
+    android.hardware.boot@1.0-impl-wrapper \
+    android.hardware.boot@1.0-impl-recovery \
+    bootctrl.msmnile \
+    bootctrl.msmnile.recovery \
+
+PRODUCT_HOST_PACKAGES += \
+    liband
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
